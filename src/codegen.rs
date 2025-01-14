83ac961c636ca44f53a9c7e36eab255e980619b2
L <lion.rexhepi@gmx.de>
2025-01-14 20:12:00 +0100

    better compiler usage

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,20 +1,23 @@
-use std::io::Write;
+#![deny(clippy::unwrap_used, unused_results)]
+use std::io::{self, Write};
 
 use crate::ir::{Ir, IrItem, Operator};
 
-pub fn write_c(program: Ir, to: &mut impl Write) {
+pub fn write_c(program: Ir, to: &mut impl Write) -> io::Result<()> {
     for item in program.items {
-        write_item(item, to);
-        to.write_all(b";").unwrap();
+        write_item(item, to)?;
+        to.write_all(b";")?;
     }
+
+    Ok(())
 }
 
-fn write_item(item: IrItem, to: &mut impl Write) {
+fn write_item(item: IrItem, to: &mut impl Write) -> io::Result<()> {
     match item {
         IrItem::Print(expr) => {
-            to.write_all(b"printf(\"%ld\\n\", ").unwrap();
-            write_item(*expr, to);
-            to.write_all(b");").unwrap();
+            to.write_all(b"printf(\"%ld\\n\", ")?;
+            write_item(*expr, to)?;
+            to.write_all(b");")
         }
         IrItem::Declaration {
             typename: _,
@@ -36,36 +39,36 @@ fn write_item(item: IrItem, to: &mut impl Write) {
                 .collect::<Vec<_>>()
                 .join(", ");
 
-            write!(to, "{} {}({}){{", return_type, var, args).unwrap();
+            write!(to, "{} {}({}){{", return_type, var, args)?;
             for item in body {
-                write_item(item, to);
-                to.write_all(b";").unwrap();
+                write_item(item, to)?;
+                to.write_all(b";")?;
             }
-            to.write_all(b"}").unwrap();
+            to.write_all(b"}")
         }
         IrItem::Declaration {
             typename: ty,
             var,
             value,
         } => {
-            write!(to, "{} {}", ty, var).unwrap();
+            write!(to, "{} {}", ty, var)?;
             if let Some(value) = value {
-                to.write_all(b" = ").unwrap();
-                write_item(*value, to);
+                to.write_all(b" = ")?;
+                write_item(*value, to)?;
             }
-            to.write_all(b";").unwrap();
+            to.write_all(b";")
         }
-        IrItem::LitInt(i) => write!(to, "{}", i).unwrap(),
-        IrItem::LitBool(b) => write!(to, "{}", b as u8).unwrap(),
-        IrItem::Variable(smol_str) => write!(to, "{}", smol_str).unwrap(),
+        IrItem::LitInt(i) => write!(to, "{}", i),
+        IrItem::LitBool(b) => write!(to, "{}", b as u8),
+        IrItem::Variable(smol_str) => write!(to, "{}", smol_str),
         IrItem::Block(block) => {
-            to.write_all(b"{").unwrap();
+            to.write_all(b"{")?;
             for item in block {
-                write_item(item, to);
+                write_item(item, to)?;
 
-                to.write_all(b";").unwrap();
+                to.write_all(b";")?;
             }
-            to.write_all(b"}").unwrap();
+            to.write_all(b"}")
         }
         IrItem::If {
             branches,
@@ -74,25 +77,26 @@ fn write_item(item: IrItem, to: &mut impl Write) {
             let len = branches.len();
             for (i, branch) in branches.into_iter().enumerate() {
                 to.write_all(b"if (").unwrap();
-                write_item(branch.condition, to);
-                to.write_all(b")").unwrap();
-                write_item(IrItem::Block(branch.then), to);
+                write_item(branch.condition, to)?;
+                to.write_all(b")")?;
+                write_item(IrItem::Block(branch.then), to)?;
 
                 if i < len - 1 {
-                    to.write_all(b"else ").unwrap();
+                    to.write_all(b"else ")?;
                 }
             }
 
             if let Some(otherwise) = otherwise {
                 to.write_all(b"else").unwrap();
-                write_item(IrItem::Block(otherwise), to);
+                write_item(IrItem::Block(otherwise), to)?;
             }
+            Ok(())
         }
         IrItem::Loop { condition, body } => {
-            to.write_all(b"while (").unwrap();
-            write_item(*condition, to);
-            to.write_all(b")").unwrap();
-            write_item(IrItem::Block(body), to);
+            to.write_all(b"while (")?;
+            write_item(*condition, to)?;
+            to.write_all(b")")?;
+            write_item(IrItem::Block(body), to)
         }
         IrItem::Function { .. } => unreachable!(),
         IrItem::Op { lhs, rhs, op } => {
@@ -110,42 +114,45 @@ fn write_item(item: IrItem, to: &mut impl Write) {
                 Operator::And => "&&",
                 Operator::Or => "||",
             };
-            write_item(*lhs, to);
+            write_item(*lhs, to)?;
             to.write_all(op.as_bytes()).unwrap();
-            write_item(*rhs, to);
+            write_item(*rhs, to)
         }
         IrItem::Assign { var, value } => {
             write!(to, "{var} = ").unwrap();
-            write_item(*value, to);
-            to.write_all(b";").unwrap();
+            write_item(*value, to)?;
+            to.write_all(b";")
         }
         IrItem::Call { name, args } => {
             write!(to, "{}(", name).unwrap();
             let len = args.len();
             for (i, arg) in args.into_iter().enumerate() {
-                write_item(arg, to);
+                write_item(arg, to)?;
                 if i < len - 1 {
                     to.write_all(b", ").unwrap();
                 }
             }
-            to.write_all(b")").unwrap();
+            to.write_all(b")")
         }
         IrItem::Multiple(items) => {
             for item in items {
-                write_item(item, to);
+                write_item(item, to)?;
             }
+            Ok(())
         }
         IrItem::Return(item) => {
             to.write_all(b"return ").unwrap();
             if let Some(val) = item {
-                write_item(*val, to);
+                write_item(*val, to)?;
             }
+            Ok(())
         }
     }
 }
 
 #[cfg(test)]
 mod test {
+    #![allow(clippy::unwrap_used)]
     use core::str;
 
     use super::*;
@@ -154,7 +161,7 @@ mod test {
     fn test_print_int() {
         let program = Ir::from([IrItem::Print(Box::new(IrItem::LitInt(42)))]);
         let mut buf = Vec::new();
-        write_c(program, &mut buf);
+        write_c(program, &mut buf).unwrap();
         assert_eq!(buf, b"printf(\"%ld\\n\", 42);");
     }
 
@@ -162,7 +169,7 @@ mod test {
     fn test_print_bool() {
         let program = Ir::from([IrItem::Print(Box::new(IrItem::LitBool(true)))]);
         let mut buf = Vec::new();
-        write_c(program, &mut buf);
+        write_c(program, &mut buf).unwrap();
         assert_eq!(str::from_utf8(&buf).unwrap(), "printf(\"%ld\\n\", 1);");
     }
 
@@ -174,7 +181,7 @@ mod test {
             value: Some(Box::new(IrItem::LitInt(42))),
         }]);
         let mut buf = Vec::new();
-        write_c(program, &mut buf);
+        write_c(program, &mut buf).unwrap();
         assert_eq!(buf, b"int foo = 42;")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,71 @@
 use core::str;
-use std::io::Write;
-
-use project_i::{
-    ast::parse,
-    ir::transformer::FileTransformer,
-    lexer::{lex, InMemoryFile},
+use std::{
+    io::{Read, Result, Write},
+    path::PathBuf,
 };
 
-fn main() {
-    let source = r#"
-        let facrec= fn facrec(n: i32): i32 {
-            if n == 2{
-                1
-            } else {
-                n * facrec (n - 1)
-            }
-        };
-        let main = fn (): i32 {
-            print facrec(4);
-        }
-        "#
-    .chars()
-    .collect::<InMemoryFile>();
-    let mut tokens = lex(source).unwrap();
-    let ast = parse(&mut tokens).unwrap();
-    let ir = project_i::ir::Ir {
-        items: FileTransformer::new().transform_file(ast).unwrap(),
-    };
-    let mut buf = Vec::from(b"#include <stdio.h>\nint add2(int a){return a+2;}\n");
-    project_i::codegen::write_c(ir, &mut buf);
-    println!("{}", str::from_utf8(&buf).unwrap());
+use clap::Parser;
+use project_i::{
+    ast::parse,
+    codegen,
+    ir::transformer::FileTransformer,
+    lexer::{lex, File, InMemoryFile},
+};
+use tempdir::TempDir;
+
+#[derive(Parser, Debug)]
+struct CompilerArgs {
+    #[arg(short, long, num_args = 1.., value_delimiter=' ', required=true )]
+    input: Vec<PathBuf>,
+    #[arg(short, long, default_value = "i.out")]
+    output: PathBuf,
+}
+
+static STD_HEADERS: &[u8] = include_bytes!("../HEADERS.h");
+
+fn main() -> Result<()> {
+    let args = CompilerArgs::parse();
+
+    let c_dir = TempDir::new("I_OUT")?;
+
+    let sources = args
+        .input
+        .into_iter()
+        .map(|path| -> Result<_> {
+            let mut string = String::new();
+            std::fs::File::open(path)?.read_to_string(&mut string)?;
+
+            let source = string.chars().collect::<InMemoryFile>();
+            let name = source.name().to_owned();
+            let mut tokens = lex(source).unwrap();
+            let ast = parse(&mut tokens).unwrap();
+            let ir = project_i::ir::Ir {
+                items: FileTransformer::new().transform_file(ast).unwrap(),
+            };
+
+            let mut c_path = c_dir.path().to_owned();
+            c_path.push(name);
+            c_path = c_path.with_extension("c");
+            println!("{}", c_path.display());
+            let mut c_file = std::fs::File::options()
+                .create(true)
+                .write(true)
+                .open(&c_path)?;
+            c_file.write_all(&STD_HEADERS)?;
+            codegen::write_c(ir, &mut c_file)?;
+
+            Ok(c_path)
+        })
+        .collect::<Result<Vec<_>>>()?;
+
     let mut gcc = std::process::Command::new("gcc")
         .arg("-x")
         .arg("c")
         .arg("-o")
-        .arg("out2")
-        .arg("-")
-        .stdin(std::process::Stdio::piped())
-        .spawn()
-        .unwrap();
-    gcc.stdin.as_mut().unwrap().write_all(&buf).unwrap();
-    gcc.wait().unwrap();
+        .arg(args.output)
+        .args(sources)
+        .spawn()?;
+    gcc.wait()?;
+
+    Ok(())
 }
